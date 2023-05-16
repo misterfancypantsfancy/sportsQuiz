5c6758861f7e483298cf16c2f337f4eef9d4af5a
misterfancypantsfancy <easyemail112233@protonmail.com>
2023-05-16 11:17:48 -0400

    WIP: preparing to integrate server side API

---
+++ api.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('Hello from /hello route!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
