31c3b3603bd300b79c016f91a1fee43651a44943
misterfancypantsfancy <easyemail112233@protonmail.com>
2023-05-16 11:41:37 -0400

    WIP: added the server side api call as a draft to api.js file

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 
+//client side api call
 const express = require('express');
 const app = express();
 
@@ -8,6 +9,30 @@ app.get('/', (req, res) => {
 
 app.get('/hello', (req, res) => {
   res.send('Hello from /hello route!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//server side api call
+const express = require('express');
+const app = express();
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from server-side API!' });
 });
 
 app.listen(3000, () => {
