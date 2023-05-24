a90a4412a9bad2e74b4870837a976e93c125440f
misterfancypantsfancy <easyemail112233@protonmail.com>
2023-05-24 11:29:29 -0400

    added express to public directory

--- server/server.js
+++ server/server.js
@@ -13,3 +13,21 @@ app.get('/api/hello', (req, res) => {
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
+
+
+
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.static('public'));
+
+// app.get('/api/hello', (req, res) => {
+//   res.json({ message: 'Hello from server-side API!' });
+// });
+
+// app.listen(3000, () => {
+//   console.log('Server started on port 3000');
+// });
