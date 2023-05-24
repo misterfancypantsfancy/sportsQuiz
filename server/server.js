8487e391167d34debe81ec5ae6a309e9b0fbf45b
misterfancypantsfancy <easyemail112233@protonmail.com>
2023-05-24 11:51:37 -0400

    public and server both have node and express, node server.js runs the quiz app and the server-side api message

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from server-side API!' });
@@ -13,21 +15,3 @@ app.get('/api/hello', (req, res) => {
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
-
-
-
-// const express = require('express');
-// const cors = require('cors');
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.static('public'));
-
-// app.get('/api/hello', (req, res) => {
-//   res.json({ message: 'Hello from server-side API!' });
-// });
-
-// app.listen(3000, () => {
-//   console.log('Server started on port 3000');
-// });
