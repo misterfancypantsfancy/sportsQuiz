0686199665514c2c63542bbd64cd4f9f6d52ed4b
misterfancypantsfancy <easyemail112233@protonmail.com>
2023-05-31 14:21:15 -0400

    updated app1 and server1 scratch pads

--- public/app1.js
+++ public/app1.js
@@ -1,162 +1,144 @@
-
-function httpPost(theUrl, payload) {
-    return fetch(theUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload)
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => data)
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
   
-  function fetchUserData() {
-    httpGet('/api/user')
-      .then(data => {
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-  
-  function sendQuizResults(answer) {
-    const payload = { result: answer };
-    httpPost('/api/user/results', payload)
-      .then(data => {
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-  
-  let results;
-  
-  function send(){
-     httpGet("https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=multiple")
-    .then(data => {
-      results = data;
-    });
-  }
-  
-  const startButton = document.getElementById("start-btn");
-  const questionContainer = document.getElementById("question-container");
-  const submitButton = document.getElementById("submit-btn");
-  const resultContainer = document.getElementById("result");
-  const restartButton = document.getElementById("restart-btn");
-  const score = document.getElementById("score");
-  
-  const questionDiv = document.getElementById("questionDiv");
-  const questionNumber = document.getElementById("questionNumber");
-  const questionText = document.getElementById("questionText");
-  const optionsDiv = document.getElementById("optionsDiv");
-  const optionDiv = document.getElementById("option");
-  
-  const dropdown = document.getElementById("userDropdown");
-  const user1 = dropdown.options[0];
-  
-  let currentQuestionIndex = 0;
-  let correctAnswers = 0;
-  
-  function renderQuestion(index) {
-    questionContainer.style.display = "block";
-  
-    let question = results.results[index];
-  
-    questionNumber.innerText = "Question " + (index + 1);
-  
-    questionText.innerHTML = question.question;
-  
-    let shuffle = [...question.incorrect_answers, question.correct_answer].sort(() => Math.random() - 0.5);
-  
-    shuffle.forEach((option, idx) => {
-      const input = document.getElementById("input" + (idx + 1));
-      const label = document.getElementById("label" + (idx + 1));
-  
-      input.setAttribute("name", "question-" + index);
-      input.setAttribute("value", option);
-      input.checked = false;
-  
-      label.innerHTML = option;
-    });
-  }
-  
-  function checkAnswer() {
-    let selectedOption = document.querySelector('input[name="question-' + currentQuestionIndex + '"]:checked');
-    if (!selectedOption) {
-      resultContainer.innerText = "Please select an answer.";
-      return;
-    }
-  
-    let answer = selectedOption.value;
-    let question = results.results[currentQuestionIndex];
-    if (answer === question.correct_answer) {
-      resultContainer.innerText = "Correct!";
-      sendQuizResults(true);
-      correctAnswers++;
-    } else {
-      resultContainer.innerText = "Incorrect. The correct answer is: " + question.correct_answer;
-      sendQuizResults(false);
-    }
-  
-    currentQuestionIndex++;
-    if (currentQuestionIndex >= results.results.length) {
-      score.innerText = "You got " + correctAnswers + " out of " + results.results.length + " questions correct!";
-      restartButton.style.display = "block";
-      score.style.display = "block";
-      submitButton.style.display = "none";
-    } else {
-      renderQuestion(currentQuestionIndex);
-    }
-  }
-  
-  function restartQuiz() {
-    currentQuestionIndex = 0;
-    correctAnswers = 0;
-    resultContainer.innerText = "";
-    restartButton.style.display = "none";
-    submitButton.style.display = "block";
-    score.style.display = "none";
-  
-    httpGet("https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=multiple")
-      .then(data => {
-        results.results = data.results;
-        renderQuestion(currentQuestionIndex);
-      });
-  }
-  
-  function submitAnswer() {
-    checkAnswer();
-  }
-  
-  submitButton.addEventListener("click", submitAnswer);
-  restartButton.addEventListener("click", restartQuiz);
-  
-  startButton.addEventListener("click", () => {
-    startButton.style.display = "none"
-    submitButton.style.display = "block"
-    renderQuestion(currentQuestionIndex)
-  })
-  
-  fetch('http://localhost:3000/api/hello')
+function httpGet(theUrl) {
+  return fetch(theUrl)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       return response.json();
     })
-    .then(data => {
-      console.log(data.message);
-    })
+    .then(data => data)
     .catch(error => {
       console.error('Error:', error);
     });
-    
+}
+
+let results;
+
+httpGet("https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=multiple")
+  .then(data => {
+    results = data;
+  });
+
+const startButton = document.getElementById("start-btn");
+const questionContainer = document.getElementById("question-container");
+const submitButton = document.getElementById("submit-btn");
+const resultContainer = document.getElementById("result");
+const restartButton = document.getElementById("restart-btn");
+const score = document.getElementById("score");
+
+const questionDiv = document.getElementById("questionDiv");
+const questionNumber = document.getElementById("questionNumber");
+const questionText = document.getElementById("questionText");
+const optionsDiv = document.getElementById("optionsDiv");
+const optionDiv = document.getElementById("option");
+
+const dropdown = document.getElementById("userDropdown");
+const user1 = dropdown.options[0];
+
+const displayDashboardButton = document.getElementById("display-dashboard-btn")
+const dashboardContainer = document.getElementById("dashboard-container");
+const quizHistory = document.getElementById("quiz-history")
+
+let currentQuestionIndex = 0;
+let correctAnswers = 0;
+
+function renderQuestion(index) {
+  questionContainer.style.display = "block";
+
+  let question = results.results[index];
+
+  questionNumber.innerText = "Question " + (index + 1);
+
+  questionText.innerHTML = question.question;
+
+  let shuffle = [...question.incorrect_answers, question.correct_answer].sort(() => Math.random() - 0.5);
+
+  shuffle.forEach((option, idx) => {
+      const input = document.getElementById("input" + (idx + 1));
+      const label = document.getElementById("label" + (idx + 1));
+
+      input.setAttribute("name", "question-" + index);
+      input.setAttribute("value", option);
+      input.checked = false;
+
+      label.innerHTML = option;
+  });
+}
+
+function checkAnswer() {
+  let selectedOption = document.querySelector('input[name="question-' + currentQuestionIndex + '"]:checked');
+  if (!selectedOption) {
+      resultContainer.innerText = "Please select an answer.";
+      return;
+  }
+
+  let answer = selectedOption.value;
+  let question = results.results[currentQuestionIndex];
+  if (answer === question.correct_answer) {
+      resultContainer.innerText = "Correct!";
+      correctAnswers++;
+  } else {
+      resultContainer.innerText = "Incorrect. The correct answer is: " + question.correct_answer;
+  }
+
+  currentQuestionIndex++;
+  if (currentQuestionIndex >= results.results.length) {
+      score.innerText = "You got " + correctAnswers + " out of " + results.results.length + " questions correct!";
+      restartButton.style.display = "block";
+      score.style.display = "block";
+      submitButton.style.display = "none";
+  } else {
+      renderQuestion(currentQuestionIndex);
+  }
+}
+
+function restartQuiz() {
+  currentQuestionIndex = 0;
+  correctAnswers = 0;
+  resultContainer.innerText = "";
+  restartButton.style.display = "none";
+  submitButton.style.display = "block";
+  score.style.display = "none";
+
+  httpGet("https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=multiple")
+      .then(data => {
+      results.results = data.results;
+      renderQuestion(currentQuestionIndex);
+      });
+}
+
+function submitAnswer() {
+  checkAnswer();
+}
+
+submitButton.addEventListener("click", submitAnswer);
+restartButton.addEventListener("click", restartQuiz);
+
+startButton.addEventListener("click", () => {
+  startButton.style.display = "none"
+  submitButton.style.display = "block"
+  renderQuestion(currentQuestionIndex)
+})
+
+displayDashboardButton.addEventListener('click', () => {
+  if (dashboardContainer.style.display === 'block') {
+    dashboardContainer.style.display = 'none';
+  } else {
+    dashboardContainer.style.display = 'block';
+  }
+});
+
+fetch('http://localhost:3000/api/hello')
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      return response.json();
+  })
+  .then(data => {
+      console.log(data.message);
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
--- server/server1.js
+++ server/server1.js
@@ -1,58 +1,17 @@
 
-// const express = require('express');
-// const cors = require('cors');
-// const path = require('path');
-// const fs = require('fs');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// app.use(cors());
-// app.use(express.static(path.join(__dirname, '../public')));
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
 
-// app.get('/api/hello', (req, res) => {
-//   res.json({ message: 'Hello from server-side API!' });
-// });
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from server-side API!' });
+});
 
-// app.listen(3000, () => {
-//   console.log('Server started on port 3000');
-// });
-
-// app.get('/api/user', (req, res) => {
-//   fs.readFile('user.json', 'utf8', (err, data) => {
-//     if (err) {
-//       console.error('Error reading user data:', err);
-//       res.sendStatus(500);
-//     } else {
-//       const userData = JSON.parse(data);
-//       res.json(userData);
-//     }
-//   });
-// });
-
-// app.post('/api/user/results', (req, res) => {
-//   const result = req.body.result;
-
-//   fs.readFile('user.json', 'utf8', (err, data) => {
-//     if (err) {
-//       console.error('Error reading user data:', err);
-//       res.sendStatus(500);
-//     } else {
-//       const userData = JSON.parse(data);
-//       if (result) {
-//         userData.right_answers++;
-//       } else {
-//         userData.wrong_answers++;
-//       }
-//       const updatedData = JSON.stringify(userData);
-
-//       fs.writeFile('user.json', updatedData, 'utf8', err => {
-//         if (err) {
-//           console.error('Error writing user data:', err);
-//           res.sendStatus(500);
-//         } else {
-//           res.sendStatus(200);
-//         }
-//       });
-//     }
-//   });
-// });
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
